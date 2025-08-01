31b9c8c3613c6c3240063330734467283424c80e
NeelakshG <neelaksh.gupta05@gmail.com>
2025-08-01 02:01:22 -0400

    Added a series of TitleCards that can be scrolled through

--- src/components/TitleCard/TitleCard.jsx
+++ src/components/TitleCard/TitleCard.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import "./TitleCard.css"
+import React, { useEffect, useRef } from "react";
+import "./TitleCard.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const cardsRef = useRef();
 
-export default TitleCard
+  // when user scrolls vertically wiht hte mouse, this handle prevents the page from vertically scrolling and instead moves the page horizontally
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="titlecards">
+      <h2>Popular on Netflix</h2>
+      {/* to imitiate the scrolling effect, we use ref instead of useState because we dont want the page to re-render. it will throw everything off loop */}
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((movie, movieIndex) => {
+          return (
+            <div className="card" key={movieIndex}>
+              <img src={movie.image} alt="" />
+              <p>{movie.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCard;
